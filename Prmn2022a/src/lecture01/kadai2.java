f718160b0c5e1ae8d04633eabbf16a3542c0cc78
showayamada <b2212580@photon.chitose.ac.jp>
2022-10-30 22:29:46 +0900

    kadai1-2

---
+++ Prmn2022a/src/lecture01/kadai2.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class kadai2 {
+    public static void main(String[] args){
+        int studentNumber = 2212580;
+        System.out.println("b" + studentNumber + "Showa Yamada");
+    }
+}
