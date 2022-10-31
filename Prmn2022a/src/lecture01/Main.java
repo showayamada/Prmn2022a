5e735f0fbbddaa87085b561e7dc69d30a5951758
showayamada <b2212580@photon.chitose.ac.jp>
2022-10-31 20:22:32 +0900

    変更

--- Prmn2022a/src/lecture01/Main.java
+++ Prmn2022a/src/lecture01/Main.java
@@ -1,6 +1,6 @@
 package lecture01;
 
-public class kadai1 {
+public class Main {
     public static void main(String[] args){
         System.out.println("B2212580 Showa Yamada");
     }
---
+++ Prmn2022a/src/lecture02/Human.java
@@ -0,0 +1,8 @@
+package lecture02;
+
+public class Human {
+    public static void main(String[] args){
+        String name;
+        int age;
+    }
+}
