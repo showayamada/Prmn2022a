4780b8bb80f5a29e5c96ec6a112fc7a462abff5f
showayamada <b2212580@photon.chitose.ac.jp>
2022-11-02 21:53:30 +0900

    lecture02課題1

---
+++ Prmn2022a/src/lecture02/Exercise2_1.java
@@ -0,0 +1,9 @@
+package lecture02;
+
+public class Exercise2_1 {
+    public static void main(String[] args){
+        Human hito = new Human();
+        hito.print();
+
+    }
+}
--- Prmn2022a/src/lecture02/Human.java
+++ Prmn2022a/src/lecture02/Human.java
@@ -1,8 +1,13 @@
 package lecture02;
 
 public class Human {
-    public static void main(String[] args){
-        String name;
-        int age;
+    String name = "たかし";
+    int age = 18;
+    public void print() {
+        if (age <= 18) {
+            System.out.println("生徒:" + name + " " + age + "歳です。");
+        } else if (age <= 22 && age > 18) {
+            System.out.println("学生：" + name + " " + age + "歳です。");
+        }
     }
 }
